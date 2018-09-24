910b4a2e998ea20e99ef302dff3ff2107e37f42f
Diego Barrios Romero <eldruin@gmail.com>
2018-09-24 17:48:13 +0200

    Add some usage examples to the documentation

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,88 @@
 //! - [PCA9548A](http://www.ti.com/lit/ds/symlink/pca9548a.pdf)
 //! 
 //! ## Usage examples (see also examples folder)
-//! TODO
+//!
+//! ### Instantiating with the default address
+//!
+//! Import this crate and an `embedded_hal` implementation, then instantiate
+//! the device:
+//!
+//! ```no_run
+//! extern crate linux_embedded_hal as hal;
+//! extern crate xca9548a;
+//!
+//! use hal::I2cdev;
+//! use xca9548a::{TCA9548A, SlaveAddr};
+//!
+//! # fn main() {
+//! let dev = I2cdev::new("/dev/i2c-1").unwrap();
+//! let address = SlaveAddr::default();
+//! let mut i2c_switch = TCA9548A::new(dev, address);
+//! # }
+//! ```
+//!
+//! ### Providing an alternative address
+//!
+//! ```no_run
+//! extern crate linux_embedded_hal as hal;
+//! extern crate xca9548a;
+//!
+//! use hal::I2cdev;
+//! use xca9548a::{TCA9548A, SlaveAddr};
+//!
+//! # fn main() {
+//! let dev = I2cdev::new("/dev/i2c-1").unwrap();
+//! let (a2, a1, a0) = (false, false, true);
+//! let address = SlaveAddr::Alternative(a2, a1, a0);
+//! let mut i2c_switch = TCA9548A::new(dev, address);
+//! # }
+//! ```
+//!
+//! ### Selecting channel 0 (SD0/SC0 pins)
+//!
+//! ```no_run
+//! extern crate linux_embedded_hal as hal;
+//! extern crate xca9548a;
+//!
+//! use hal::I2cdev;
+//! use xca9548a::{TCA9548A, SlaveAddr};
+//!
+//! # fn main() {
+//! let dev = I2cdev::new("/dev/i2c-1").unwrap();
+//! let address = SlaveAddr::default();
+//! let mut i2c_switch = TCA9548A::new(dev, address);
+//! i2c_switch.select(0b0000_0001).unwrap();
+//! # }
+//! ```
+//! ### Reading and writing to device connected to channel 0 (SD0/SC0 pins)
+//!
+//! ```no_run
+//! extern crate embedded_hal;
+//! extern crate linux_embedded_hal as hal;
+//! extern crate xca9548a;
+//!
+//! use hal::I2cdev;
+//! use embedded_hal::blocking::i2c::{ Read, Write };
+//! use xca9548a::{ TCA9548A, SlaveAddr };
+//!
+//! # fn main() {
+//! let dev = I2cdev::new("/dev/i2c-1").unwrap();
+//! let address = SlaveAddr::default();
+//! let mut i2c_switch = TCA9548A::new(dev, address);
+//! i2c_switch.select(0b0000_0001).unwrap();
+//!
+//! let slave_address = 0b010_0000; // example slave address
+//! let data_for_slave = [0b0101_0101, 0b1010_1010]; // some data to be sent
+//! 
+//! // Read some data from a slave connected to channel 0 using the
+//! // I2C switch just as a normal I2C device
+//! let mut read_data = [0; 2];
+//! i2c_switch.read(slave_address, &mut read_data).unwrap();
+//!
+//! // Write some data to the slave
+//! i2c_switch.write(slave_address, &data_for_slave).unwrap();
+//! # }
+//! ```
 //!
 
 #![deny(unsafe_code)]
