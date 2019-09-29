d612d23f50333cbaaf0b2ed3ea0530e72d392a30
Diego Barrios Romero <eldruin@gmail.com>
2019-09-29 20:58:02 +0200

    Improve example

--- src/lib.rs
+++ src/lib.rs
@@ -107,6 +107,9 @@
 //!
 //! ### Splitting into individual I2C devices
 //!
+//! It does not matter if the slaves have the same address.
+//! Switching will be done automatically as necessary.
+//!
 //! ```no_run
 //! extern crate embedded_hal;
 //! extern crate linux_embedded_hal as hal;
@@ -121,8 +124,8 @@
 //! let i2c_switch = TCA9548A::new(dev, address);
 //! let mut parts = i2c_switch.split();
 //!
-//! let slave_address = 0b010_0000; // example slave address
-//! let data_for_slave = [0b0101_0101, 0b1010_1010]; // some data to be sent
+//! let slave_address = 0x20;
+//! let data_for_slave = [0xAB, 0xCD];
 //!
 //! // Write some data to the slave using normal I2C interface
 //! parts.i2c0.write(slave_address, &data_for_slave).unwrap();
